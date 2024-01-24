02c64c15520102c9e3f91da5e360ee0b4f65c763
Lenovo-User <Lenovo-User@DESKTOP-KGOC38Q>
2024-01-24 18:22:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_calculator_with_user_s_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a niumber");
+            double num1 = Convert.ToDouble (Console.ReadLine());
+
+            Console.WriteLine("Enter an operator");
+            String op = Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+            double num2 = Convert.ToDouble (Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+
+            }else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Not an operator");
+            }
+
+            Console.Read();
+             
+        }
+    }
+}
